7090f3240acab45a5e9b9bf65b29643b6cbe5111
Lewen Feng <lewfeng2569@gmail.com>
2023-07-05 18:48:51 -0500

    First Commit

--- ALP/tools/clear.py
+++
@@ -1,6 +0,0 @@
-from tools import prettier, requirements_txt
-
-
-def code_format_and_make_requirements_txt(path):
-    prettier.code_format(path)
-    requirements_txt.make_requirements_txt(path)
--- ALP/utils/global_variables.py
+++
@@ -1,4 +0,0 @@
-Timer_BOOL = True
-numOfTests = 6
-start_tick = 0
-tmpBool = True
